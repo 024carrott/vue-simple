2da06ad686e296f17d5f827f2f79315fabfa52e2
Jee Hoon <yamoo9@naver.com>
2017-07-27 15:10:52 +0900

    Assets 설정 관련하여 주석 추가

--- template/src/App.vue
+++ template/src/App.vue
@@ -2,6 +2,11 @@
 {{#if pug}}
   #app
     img(:src="vue.path", :alt="vue.label")
+
+    //- 직접 에셋 속성을 설정하는 경우, assets/ 디렉토리에서 찾아야 이미지 출력
+    //- file-loader를 통해 아래와 같이 처리되기 때문
+    //- /dist/logo.png?82b9c7a5a3f405032b1db71a25f67021
+    //- img(src="./assets/logo.png", :alt="vue.label")
 {{else}}
   <div id="app">
     <img :src="vue.path" :alt="vue.label">
@@ -15,7 +20,9 @@ export default {
   data () {
     return {
       vue: {
-        path: './assets/logo.png',
+        // 동적 속성 바인딩 시에는 src/ 디렉토리에서 찾아야 이미지 출력
+        // file-loader를 사용하지 않고, 직접 속성 값을 설정하기 때문
+        path: './src/assets/logo.png',
         label: 'Vue.js'
       }
     }
